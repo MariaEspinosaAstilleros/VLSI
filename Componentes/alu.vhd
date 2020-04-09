6f2b24f4379c63d10a4b4468ff0f3215320f5441
MariaEspinosaAstilleros <43780193+MariaEspinosaAstilleros@users.noreply.github.com>
2020-04-09 22:00:00 +0200

    Implementacion añadida en lab 7

--- Componentes/alu.vhd
+++ Componentes/alu.vhd
@@ -10,15 +10,15 @@ ENTITY alu IS
     PORT(
         A:  IN STD_LOGIC_VECTOR(1 TO 4);         -- primer operando
         B:  IN STD_LOGIC_VECTOR(5 TO 8);         -- segundo operando
-        Op: IN STD_LOGIC_VECTOR(2 DOWNTO 0);     -- entrada de selección 
+        s:  IN STD_LOGIC_VECTOR(2 DOWNTO 0);     -- entrada de selección 
         F:  OUT STD_LOGIC_VECTOR(3 DOWNTO 0));   -- salida 
 END alu; 
 
 ARCHITECTURE aluFunc OF alu IS 
 BEGIN 
-    PROCESS(Op, A, B)
+    PROCESS(s, A, B)
     BEGIN 
-        CASE Op IS 
+        CASE s IS 
             WHEN "000" => F <= "0000";
             WHEN "001" => F <= B - A;
             WHEN "010" => F <= A - B;
--- Proyectos/lab7/ALU74381/lab7.vhd
+++ Proyectos/lab7/ALU74381/lab7.vhd
@@ -8,15 +8,47 @@ USE ieee.std_logic_unsigned.all;
 
 ENTITY lab7 IS 
     PORT(
-        DIP_SW: IN STD_LOGIC_VECTOR(1 TO 8);  -- dip switches externos
+        DIP_SW: IN STD_LOGIC_VECTOR(1 TO 8);   -- dip switches externos
         SW: IN STD_LOGIC_VECTOR(2 DOWNTO 0);   -- dip siwtches DE0-nano
         KEY_EX: IN STD_LOGIC_VECTOR(0 TO 1);   -- pulsadores
         LED: OUT STD_LOGIC_VECTOR(3 DOWNTO 0); -- leds
-        DISP: OUT STD_LOGIC_VECTOR(1 TO 7));   -- display 7 segmentos 
+        DISP: OUT STD_LOGIC_VECTOR(1 TO 7);    -- display 7 segmentos 
+        R: OUT STD_LOGIC); 
 END lab7; 
 
 
-ARCHITECTURE funcLab7 OF lab7 IS 
-BEGIN 
+ARCHITECTURE funcLab7 OF lab7 IS  
+SIGNAL l : STD_LOGIC_VECTOR(3 DOWNTO 0); 
+-- componente de la alu
+COMPONENT alu 
+    PORT(
+        A:  IN STD_LOGIC_VECTOR(1 TO 4);        
+        B:  IN STD_LOGIC_VECTOR(5 TO 8);         
+        s:  IN STD_LOGIC_VECTOR(2 DOWNTO 0);     
+        F:  OUT STD_LOGIC_VECTOR(3 DOWNTO 0));
+END COMPONENT; 
 
+--componente del display de 7 segmentos
+COMPONENT disp_seg7  
+    PORT(
+        result :  IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        display : OUT STD_LOGIC_VECTOR(1 TO 7));
+END COMPONENT;
+
+
+BEGIN
+            F => l;
+    a : alu port map(
+        A => DIP_SW(1 TO 4), B => DIP_SW(5 TO 8), s => SW(2 DOWNTO 0), l => LED(3 DOWNTO 0)
+    ); 
+
+    d : disp_seg7 port map(
+        result => l, display => DISP(1 TO 7)
+    );
+
+    WITH KEY_EX SELECT
+        R <= F WHEN "00",        -- muestra el resultado de la operacion
+             B WHEN "01",        -- muestra el operando B
+             A WHEN "10",        -- muestra el operando A
+             s WHEN OTHERS;      -- muestra las entradas de seleccion
 END funcLab7;
