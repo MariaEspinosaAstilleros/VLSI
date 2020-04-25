7bff99312d8e951d6076618e378f0f56e6636258
MariaEspinosaAstilleros <43780193+MariaEspinosaAstilleros@users.noreply.github.com>
2020-04-25 17:03:01 +0200

    Código comentado

--- Proyectos/lab8/ContadorAsincrono/contador_asincrono.vhd
+++ Proyectos/lab8/ContadorAsincrono/contador_asincrono.vhd
@@ -22,19 +22,19 @@ ARCHITECTURE lab8 OF contador_asincrono IS
     SIGNAL cnt: INTEGER RANGE 0 TO 15; 
     SIGNAL load: STD_LOGIC; 
 BEGIN 
-    PROCESS(clk, resetn, load)                              -- resetn activa el proceso de modo asincrono
+    PROCESS(clk, resetn, load)                              
     BEGIN 
-        IF resetn = '0' THEN                          -- resetn activo a nivel bajo 
+        IF resetn = '0' THEN                           
             cnt <= 0;                                 -- se reinicia el contador     
-        ELSIF(clk'EVENT AND clk = '1') THEN           -- flanco de subida del reloj y enable activado
+        ELSIF(clk'EVENT AND clk = '1') THEN           -- flanco de subida del reloj 
             IF load = '1' THEN 
-                cnt <= 0;                                -- cargo en el contador el valor que tenga el registro R
+                cnt <= 0;                             -- si ha llegado al máximo valor reinicio el contador
             ELSIF en = '0' THEN 
-                cnt <= cnt;                      -- activo la señal de habilitacio                        -- aumento a 1 el contado 
-            ELSIF ld = '0' AND en = '1' THEN                          -- carga activa a nivel alto
-                cnt <= cnt + 1; 
+                cnt <= cnt;                           -- si enable no está activado mantengo el valor del contador                     
+            ELSIF ld = '0' AND en = '1' THEN                         
+                cnt <= cnt + 1;                       -- si enable está activo y la señal de carga no está activada se aumenta el valor del contador a 1 
             ELSIF ld = '1' THEN  
-                cnt <= R;                          -- si el enable es 0 mantengo el valor del contador  
+                cnt <= R;                             -- cargo el valor que contenga R
             END IF;     
         END IF; 
     END PROCESS; 
@@ -43,12 +43,12 @@ BEGIN
     PROCESS(cnt, max)
     BEGIN 
         load <= '0'; 
-        IF (cnt = max) THEN                            -- llego al valor máximo 
-            load <= '1';                               -- activo el indicador fin de cuenta  
+        IF (cnt = max) THEN                           -- llego al valor máximo 
+            load <= '1';                              -- activo el indicador fin de cuenta  
         END IF; 
     END PROCESS; 
 
-    cout <= load; 
-    Q <= cnt;                                          -- asigno el signal cnt al contador
+    cout <= load;                                     -- asigno el signal load al indicafor fin de cuenta
+    Q <= cnt;                                         -- asigno el signal cnt al contador
 END lab8; 
     
